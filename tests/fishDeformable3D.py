606c5d9aa1892e075aa66c25d52aa9325df6b685
Siavash Khallaghi <siavash@Siavashs-MacBook-Pro.local>
2017-03-02 10:13:02 -0800

    added the test script file

---
+++ tests/fishDeformable3D.py
@@ -0,0 +1,35 @@
+from functools import partial
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from core import DeformableRegistration
+import numpy as np
+import time
+
+def visualize(X, Y, ax):
+    plt.cla()
+    ax.scatter(X[:,0],  X[:,1], X[:,2], color='red')
+    ax.scatter(Y[:,0],  Y[:,1], Y[:,2], color='blue')
+    plt.draw()
+    plt.pause(0.001)
+
+def main():
+    fish = loadmat('./data/fish.mat')
+    X = np.zeros((fish['X'].shape[0], fish['X'].shape[1] + 1))
+    X[:,:-1] = fish['X']
+
+    Y = np.zeros((fish['Y'].shape[0], fish['Y'].shape[1] + 1))
+    Y[:,:-1] = fish['Y']
+
+    print X.shape, Y.shape
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    callback = partial(visualize, ax=ax)
+
+    reg = DeformableRegistration(X, Y)
+    reg.register(callback)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
