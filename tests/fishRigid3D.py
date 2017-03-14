f2f6f6ec6ed3ea3bfb03dcec1ac070bc4f22ab26
Siavash Khallaghi <siavash@Siavashs-MacBook-Pro.local>
2017-03-14 12:04:43 -0700

    added tests for 2D and 3D point cloud registration

---
+++ tests/fishRigid3D.py
@@ -0,0 +1,33 @@
+from functools import partial
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from core import RigidRegistration
+import numpy as np
+import time
+
+def visualize(X, Y, ax):
+    plt.cla()
+    ax.scatter(X[:,0],  X[:,1], X[:,2], color='red')
+    ax.scatter(Y[:,0],  Y[:,1], Y[:,2], color='blue')
+    plt.draw()
+    plt.pause(0.001)
+
+def main():
+    fish = loadmat('./data/fish.mat')
+    X = np.zeros((fish['X'].shape[0], fish['X'].shape[1] + 1))
+    X[:,:-1] = fish['X']
+
+    Y = np.zeros((fish['Y'].shape[0], fish['Y'].shape[1] + 1))
+    Y[:,:-1] = fish['Y']
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    callback = partial(visualize, ax=ax)
+
+    reg = RigidRegistration(X, Y)
+    reg.register(callback)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
