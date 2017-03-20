bf4161b9901f488b61e787a1b081aae116e626d3
Siavash Khallaghi <siavash@dhcp-128-189-220-204.ubcsecure.wireless.ubc.ca>
2017-03-19 18:37:53 -0700

    added an example for rigid bunny registration, updated the bunny to a lower resolution version to speed up the registration

---
+++ examples/bunnyRigid3D.py
@@ -0,0 +1,31 @@
+from functools import partial
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from pycpd import rigid_registration
+import numpy as np
+import time
+
+def visualize(iteration, error, X, Y, ax):
+    plt.cla()
+    ax.scatter(X[:,0],  X[:,1], X[:,2], color='red')
+    ax.scatter(Y[:,0],  Y[:,1], Y[:,2], color='blue')
+    plt.draw()
+    print "iteration %d, error %.5f" % (iteration, error)
+    plt.pause(0.001)
+
+def main():
+    fish = loadmat('./data/bunny.mat')
+    X = fish['X']
+    Y = X + 1
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    callback = partial(visualize, ax=ax)
+
+    reg = rigid_registration(X, Y)
+    reg.register(callback)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
