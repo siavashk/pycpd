31041d941f4f8e83c6b1992bf38839adc1f78e9c
Siavash Khallaghi <siavashk@ece.ubc.ca>
2017-06-13 16:35:47 -0700

    skeleton for jrmpc rigid registration

---
+++ examples/fishJRMPC2D.py
@@ -0,0 +1,29 @@
+from functools import partial
+from scipy.io import loadmat
+import matplotlib.pyplot as plt
+from pycpd import jrmpc_rigid
+import numpy as np
+import time
+
+def visualize(iteration, error, X, Y, ax):
+    plt.cla()
+    ax.scatter(X[:,0] ,  X[:,1], color='red')
+    ax.scatter(Y[:,0] ,  Y[:,1], color='blue')
+    plt.draw()
+    print "iteration %d, error %.5f" % (iteration, error)
+    plt.pause(0.001)
+
+def main():
+    fish = loadmat('./data/fish.mat')
+    X = fish['X']
+    Y = fish['Y']
+
+    fig = plt.figure()
+    fig.add_axes([0, 0, 1, 1])
+    callback = partial(visualize, ax=fig.axes[0])
+
+    reg = jrmpc_rigid([X, Y])
+    reg.print_self()
+
+if __name__ == '__main__':
+    main()
