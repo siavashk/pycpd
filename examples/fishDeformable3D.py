9172328e20fba74393649cb6694a674357fd672b
mhr <mhr@users.noreply.github.com>
2018-01-20 00:55:48 -0500

    Fixed a typo

--- examples/fishDeformable3D.py
+++ examples/fishDeformable3D.py
@@ -15,7 +15,7 @@ def visualize(iteration, error, X, Y, ax):
     plt.pause(0.001)
 
 def main():
-    fish = loadmat('.,/data/fish.mat')
+    fish = loadmat('../data/fish.mat')
     X = np.zeros((fish['X'].shape[0], fish['X'].shape[1] + 1))
     X[:,:-1] = fish['X']
 
