0661794f4d77c9c2814ed0f7d26a03e10edc762f
Siavash Khallaghi <siavashk@ece.ubc.ca>
2018-08-07 14:30:41 -0700

    Displaying iteration and error in the same matplotlib figure as opposed to the terminal.

--- examples/fish_rigid_2D.py
+++ examples/fish_rigid_2D.py
@@ -7,10 +7,11 @@ import time
 
 def visualize(iteration, error, X, Y, ax):
     plt.cla()
-    ax.scatter(X[:,0] ,  X[:,1], color='red')
-    ax.scatter(Y[:,0] ,  Y[:,1], color='blue')
+    ax.scatter(X[:,0] ,  X[:,1], color='red', label='Target')
+    ax.scatter(Y[:,0] ,  Y[:,1], color='blue', label='Source')
+    plt.text(0.87, 0.92, 'Iteration: {:d}\nError: {:06.4f}'.format(iteration, error), horizontalalignment='center', verticalalignment='center', transform=ax.transAxes, fontsize='x-large')
+    ax.legend(loc='upper left', fontsize='x-large')
     plt.draw()
-    print("iteration %d, error %.5f" % (iteration, error))
     plt.pause(0.001)
 
 def main():
--- examples/fish_rigid_3D.py
+++ examples/fish_rigid_3D.py
@@ -8,10 +8,11 @@ import time
 
 def visualize(iteration, error, X, Y, ax):
     plt.cla()
-    ax.scatter(X[:,0],  X[:,1], X[:,2], color='red')
-    ax.scatter(Y[:,0],  Y[:,1], Y[:,2], color='blue')
+    ax.scatter(X[:,0],  X[:,1], X[:,2], color='red', label='Target')
+    ax.scatter(Y[:,0],  Y[:,1], Y[:,2], color='blue', label='Source')
+    ax.text2D(0.87, 0.92, 'Iteration: {:d}\nError: {:06.4f}'.format(iteration, error), horizontalalignment='center', verticalalignment='center', transform=ax.transAxes, fontsize='x-large')
+    ax.legend(loc='upper left', fontsize='x-large')
     plt.draw()
-    print("iteration %d, error %.5f" % (iteration, error))
     plt.pause(0.001)
 
 def main():
