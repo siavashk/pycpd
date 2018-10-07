Added more tests. Fixed an issue where the rotation and affine registration results were returned as transpose.

pycpd/affine_registration.py
@@ -21,23 +21,22 @@ class affine_registration(expectation_maximization_registration):
         self.YPY = np.dot(np.transpose(YY), np.diag(self.P1))
         self.YPY = np.dot(self.YPY, YY)
 
-        Bt = np.linalg.solve(np.transpose(self.YPY), np.transpose(self.A))
-        self.B = np.transpose(Bt)
-        self.t = np.transpose(muX) - np.dot(self.B, np.transpose(muY))
+        self.B = np.linalg.solve(np.transpose(self.YPY), np.transpose(self.A))
+        self.t = np.transpose(muX) - np.dot(np.transpose(self.B), np.transpose(muY))
 
     def transform_point_cloud(self, Y=None):
         if Y is None:
-            self.TY = np.dot(self.Y, np.transpose(self.B)) + np.tile(self.t, (self.M, 1))
+            self.TY = np.dot(self.Y, self.B) + np.tile(self.t, (self.M, 1))
             return
         else:
-            return np.dot(Y, np.transpose(self.B)) + np.tile(self.t, (Y.shape[0], 1))
+            return np.dot(Y, self.B) + np.tile(self.t, (Y.shape[0], 1))
 
     def update_variance(self):
         qprev = self.q
 
-        trAB     = np.trace(np.dot(self.A, np.transpose(self.B)))
+        trAB     = np.trace(np.dot(self.A, self.B))
         xPx      = np.dot(np.transpose(self.Pt1), np.sum(np.multiply(self.XX, self.XX), axis =1))
-        trBYPYP  = np.trace(np.dot(np.dot(self.B, self.YPY), np.transpose(self.B)))
+        trBYPYP  = np.trace(np.dot(np.dot(self.B, self.YPY), self.B))
         self.q   = (xPx - 2 * trAB + trBYPYP) / (2 * self.sigma2) + self.D * self.Np/2 * np.log(self.sigma2)
         self.err = np.abs(self.q - qprev)
 

pycpd/deformable_registration.py
@@ -2,7 +2,7 @@ from builtins import super
 import numpy as np
 from .expectation_maximization_registration import expectation_maximization_registration
 
-def make_kernel(Y, beta):
+def gaussian_kernel(Y, beta):
     (M, D) = Y.shape
     XX = np.reshape(Y, (1, M, D))
     YY = np.reshape(Y, (M, 1, D))
@@ -19,7 +19,7 @@ class deformable_registration(expectation_maximization_registration):
         self.alpha         = 2 if alpha is None else alpha
         self.beta          = 2 if alpha is None else beta
         self.W             = np.zeros((self.M, self.D))
-        self.G             = make_kernel(self.Y, self.beta)
+        self.G             = gaussian_kernel(self.Y, self.beta)
 
     def update_transform(self):
         A = np.dot(np.diag(self.P1), self.G) + self.alpha * self.sigma2 * np.eye(self.M)

pycpd/rigid_registration.py
@@ -28,22 +28,22 @@ class rigid_registration(expectation_maximization_registration):
         C = np.ones((self.D, ))
         C[self.D-1] = np.linalg.det(np.dot(U, V))
 
-        self.R = np.dot(np.dot(U, np.diag(C)), V)
+        self.R = np.transpose(np.dot(np.dot(U, np.diag(C)), V))
         self.YPY = np.dot(np.transpose(self.P1), np.sum(np.multiply(YY, YY), axis=1))
-        self.s = np.trace(np.dot(np.transpose(self.A), self.R)) / self.YPY
-        self.t = np.transpose(muX) - self.s * np.dot(self.R, np.transpose(muY))
+        self.s = np.trace(np.dot(np.transpose(self.A), np.transpose(self.R))) / self.YPY
+        self.t = np.transpose(muX) - self.s * np.dot(np.transpose(self.R), np.transpose(muY))
 
     def transform_point_cloud(self, Y=None):
         if Y is None:
-            self.TY = self.s * np.dot(self.Y, np.transpose(self.R)) + np.tile(self.t, (self.M, 1))
+            self.TY = self.s * np.dot(self.Y, self.R) + np.tile(self.t, (self.M, 1))
             return
         else:
-            return self.s * np.dot(Y, np.transpose(self.R)) + np.tile(np.transpose(self.t), (Y.shape[0], 1))
+            return self.s * np.dot(Y, self.R) + np.tile(np.transpose(self.t), (Y.shape[0], 1))
 
     def update_variance(self):
         qprev = self.q
 
-        trAR = np.trace(np.dot(self.A, np.transpose(self.R)))
+        trAR = np.trace(np.dot(self.A, self.R))
         xPx = np.dot(np.transpose(self.Pt1), np.sum(np.multiply(self.XX, self.XX), axis =1))
         self.q = (xPx - 2 * self.s * trAR + self.s * self.s * self.YPY) / (2 * self.sigma2) + self.D * self.Np/2 * np.log(self.sigma2)
         self.err = np.abs(self.q - qprev)