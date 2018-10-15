2e2c50d7cc649605c4b0f249802b7becd18e71d1
Siavash Khallaghi <siavash.khallaghi@clarius.me>
2018-10-15 14:53:50 -0700

    Updated contributors in THANKS.txt. Removed JRMPC from repository as I do not have bandwidth for it right now.

--- pycpd/__init__.py
+++ pycpd/__init__.py
@@ -1,4 +1,3 @@
 from .affine_registration import affine_registration
 from .rigid_registration import rigid_registration
 from .deformable_registration import gaussian_kernel, deformable_registration
-from .jrmpc_rigid import jrmpc_rigid
